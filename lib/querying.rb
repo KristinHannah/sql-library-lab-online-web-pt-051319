6bd5e56f528fb597a82b2c20d0bcda1f3f167d3f
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-23 22:41:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -57,7 +57,6 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(IF(characters.id = character_books.character_id, 1, NULL)) AS book_count
   FROM character_books
   INNER JOIN characters
-  ON characters.id = character_books.character_id
-  WHERE characters.id = character_books.character_id;
-  "
+  ON characters.id = character_books.character_id;"
+  #WHERE characters.id = character_books.character_id;
 end
