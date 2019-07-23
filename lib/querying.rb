11b02ffa6e2cb3870623fb9d918a4038e6bb9405
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-23 22:32:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -46,8 +46,7 @@ def select_series_title_with_most_human_characters
   FROM characters
   INNER JOIN series 
   ON characters.series_id = series.id
-  WHERE COUNT(characters.species = 'human')
-  GROUP BY characters.species;
+  HAVING MAX(characters.species = 'human');
   "
 end
     
